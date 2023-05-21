08875ce8aabeaabe99104416d3aae563061c6413
Justin Choi <justin7623@gmail.com>
2023-05-21 17:48:30 +0800

    Added RabbitMQ

---
+++ amqp/src/main/java/com/justinchoi/amqp/RabbitMQConfig.java
@@ -0,0 +1,30 @@
+package com.justinchoi.amqp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQConfig {
+
+    private final ConnectionFactory connectionFactory;
+
+    @Bean
+    public AmqpTemplate amqpTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter jacksonMessageConverter() {
+        MessageConverter messageConverter = new Jackson2JsonMessageConverter();
+        return messageConverter;
+    }
+}
